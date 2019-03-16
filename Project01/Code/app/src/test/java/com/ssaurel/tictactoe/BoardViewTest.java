f9c07376c0bf872cea9d73977cd262c77073f4b2
Muhammad Sakib Khan <Muhammad.sakib@northsouth.edu>
2019-03-17 03:52:08 +0600

    Test Java Files Created

---
+++ Project01/Code/app/src/test/java/com/ssaurel/tictactoe/BoardViewTest.java
@@ -0,0 +1,31 @@
+package com.ssaurel.tictactoe;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Khan Desktop on 17-Mar-19.
+ */
+public class BoardViewTest {
+    @Test
+    public void setMainActivity() throws Exception {
+    }
+
+    @Test
+    public void setGameEngine() throws Exception {
+    }
+
+    @Test
+    public void onMeasure() throws Exception {
+    }
+
+    @Test
+    public void onDraw() throws Exception {
+    }
+
+    @Test
+    public void onTouchEvent() throws Exception {
+    }
+
+}
---
+++ Project01/Code/app/src/test/java/com/ssaurel/tictactoe/MainActivityTest.java
@@ -0,0 +1,27 @@
+package com.ssaurel.tictactoe;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Khan Desktop on 17-Mar-19.
+ */
+public class MainActivityTest {
+    @Test
+    public void onCreate() throws Exception {
+    }
+
+    @Test
+    public void onCreateOptionsMenu() throws Exception {
+    }
+
+    @Test
+    public void onOptionsItemSelected() throws Exception {
+    }
+
+    @Test
+    public void gameEnded() throws Exception {
+    }
+
+}
