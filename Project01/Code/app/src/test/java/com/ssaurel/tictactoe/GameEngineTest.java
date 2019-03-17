ffd11d4d62b33d9630edd0686928e8a3b0393998
Muhammad Sakib Khan <Muhammad.sakib@northsouth.edu>
2019-03-17 21:11:30 +0600

    Testing of play() method of GameEngine class done

--- Project01/Code/app/src/test/java/com/ssaurel/tictactoe/GameEngineTest.java
+++ Project01/Code/app/src/test/java/com/ssaurel/tictactoe/GameEngineTest.java
@@ -2,6 +2,8 @@ package com.ssaurel.tictactoe;
 
 import org.junit.Test;
 
+import java.util.Random;
+
 import static org.junit.Assert.*;
 
 /**
@@ -10,14 +12,34 @@ import static org.junit.Assert.*;
 public class GameEngineTest {
     @Test
     public void isEnded() throws Exception {
+
     }
+
+
 
     @Test
     public void play() throws Exception {
+
+        final Random RANDOM = new Random();
+        char[] elts = new char[9];
+        char currentPlayer= 'X';
+        boolean ended = false;
+        int input1 = 2;
+        int input2 = 1;
+        char expected= ' ' ;
+        char output;
+
+
+        GameEngine checkplay = new GameEngine();
+        output = checkplay.play(input1,input2);
+
+        assertEquals(expected, output); //passed test as the input values allow method to return successfully
+
     }
 
     @Test
     public void changePlayer() throws Exception {
+
     }
 
     @Test
